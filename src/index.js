9a96890912a1bca2a0a521c4a07934d8d48feb10
Vladislav-Fedurkin <vladislav.fedurkin@gmail.com>
2021-03-15 22:44:10 +0300

    feat: add solution to task Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  const str = number.toString();
+  if (parseInt(str) === 0) {
+    return 'zero';
+  }
+
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const words = [];
+  const integers = str.split('').reverse().map(parseFloat);
+  if (integers[1] === 1) {
+    integers[0] += 10;
+  }
+
+  let word = units[integers[0]];
+  if (word) {
+    words.push(word);
+  }
+
+  word = tens[integers[1]];
+  if (word) {
+    words.push(word);
+  }
+
+  word = units[integers[2]];
+  if (word) {
+    words.push(word + ' hundred');
+  }
+
+  return words.reverse().join(' ');
 }
